623a0b45167ba5292a4b5c3c91c073e4f9bef277
thiakovidis <thiakovidis@agroapps.gr>
2022-12-04 17:23:58 +0200

    Add menu items in the main navigation menu with test

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -4,7 +4,22 @@
       <div
         class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8"
       >
-        <a class="flex h-full items-center text-xl" href="/">{{ company }}</a>
+        <a class="flex h-full items-center text-xl" :href="url">{{
+          company
+        }}</a>
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full list-none">
+            <li
+              v-for="menuItem in menuItems"
+              :key="menuItem"
+              class="ml-9 h-full first:ml-0"
+            >
+              <a href="#" class="flex h-full items-center py-2.5">{{
+                menuItem
+              }}</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -16,6 +31,14 @@ export default {
   data() {
     return {
       company: "Bobo Careers",
+      url: "https://careers.google.com",
+      menuItems: [
+        "Teams ",
+        "Location",
+        "Life at Bobo Corp",
+        "Students",
+        "Jobs",
+      ],
     };
   },
 };
--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -7,4 +7,21 @@ describe("MainNav", () => {
     const companyName = screen.getByText("Bobo Careers");
     expect(companyName).toBeInTheDocument();
   });
+
+  it("displays meu items for navigation", () => {
+    render(MainNav);
+    const navigationMenuItems = screen.getAllByRole("listitem");
+    // console.log(navigationMenuItems);
+    const navigationMenuTexts = navigationMenuItems.map(
+      (item) => item.textContent
+    );
+    // console.log(navigationMenuTexts);
+    expect(navigationMenuTexts).toEqual([
+      "Teams ",
+      "Location",
+      "Life at Bobo Corp",
+      "Students",
+      "Jobs",
+    ]);
+  });
 });
