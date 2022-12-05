67c73fba096f62253563e70303d478f2fcbe9061
thiakovidis <thiakovidis@agroapps.gr>
2022-12-05 22:38:53 +0200

    Add job search form component

---
+++ src/components/JobSearchForm.vue
@@ -0,0 +1,14 @@
+<template>
+  <form
+    class="flex h-12 w-full items-center rounded-3xl border border-solid border-brand-gray-3"
+  >
+    <font-awesome-icon :icon="['fas', 'search']" class="ml-4 mr-3" />
+    <div class="flex h-full flex-1 flex-nowrap text-base font-light"></div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "JobSearchForm",
+};
+</script>
