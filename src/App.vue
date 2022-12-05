f8047132a775153fbb98467264f0322c56510ee5
thiakovidis <thiakovidis@agroapps.gr>
2022-12-05 11:41:26 +0200

    Create hero component with basic UI configuration

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <main-nav />
+  <the-hero />
 </template>
 
 <script>
 import MainNav from "@/components/MainNav.vue";
+import TheHero from "@/components/TheHero.vue";
 
 export default {
   name: "App",
-  components: { MainNav },
+  components: { MainNav, TheHero },
 };
 </script>
---
+++ src/components/TheHero.vue
@@ -0,0 +1,26 @@
+<template>
+  <main>
+    <section class="flex h-screen flex-col bg-yellow-200 pt-10 pb-20">
+      <div class="grid grid-cols-12">
+        <div class="col-span-1 col-start-1 border border-blue-700">
+          1 Column
+        </div>
+        <div class="col-span-5 col-start-2 border border-blue-700">
+          5 Columns
+        </div>
+        <div class="col-span-5 col-start-7 border border-blue-700">
+          5 columns
+        </div>
+        <div class="col-span-1 col-start-12 border border-blue-700">
+          1 Column
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "TheHero",
+};
+</script>
