307681a27fa15094a6db0c1b6c3808e50516e346
thiakovidis <thiakovidis@agroapps.gr>
2022-12-05 13:05:55 +0200

    Add tests for the headline component

---
+++ tests/unit/components/TheHeadline.test.js
@@ -0,0 +1,57 @@
+import { render, screen } from "@testing-library/vue";
+import TheHeadline from "@/components/TheHeadline.vue";
+import { nextTick } from "vue";
+
+describe("TheHeadline", () => {
+  // describe("Vitest playground", () => {
+  //   it("tracks whether it has been called ", () => {
+  //     const mockFunction = vi.fn();
+  //     mockFunction(1, 2);
+  //     expect(mockFunction).toHaveBeenCalledWith(1, 2);
+  //   });
+  // });
+  beforeEach(() => {
+    vi.useFakeTimers();
+  });
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
+  it("displays introductory actin verb", () => {
+    render(TheHeadline);
+    const actionPhrase = screen.getByRole("heading", {
+      name: /build for everyone/i,
+    });
+
+    expect(actionPhrase).toBeInTheDocument();
+  });
+
+  it("changes action verb at a consistent interval", () => {
+    const mock = vi.fn();
+    vi.stubGlobal("setInterval", mock);
+    render(TheHeadline);
+
+    expect(mock).toHaveBeenCalled();
+  });
+
+  it("swaps action verb after interval", async () => {
+    render(TheHeadline);
+    vi.advanceTimersToNextTimer();
+    await nextTick();
+
+    const actionPhrase = screen.getByRole("heading", {
+      name: /create for everyone/i,
+    });
+
+    expect(actionPhrase).toBeInTheDocument();
+  });
+
+  it("removes interval when component dissapears", () => {
+    const clearInterval = vi.fn();
+    vi.stubGlobal("clearInterval", clearInterval);
+    const { unmount } = render(TheHeadline);
+    unmount();
+
+    expect(clearInterval).toHaveBeenCalled();
+  });
+});
