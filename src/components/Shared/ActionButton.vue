1328b43546ef9c41b8edb9b2ec547d1263a735cb
thiakovidis <thiakovidis@agroapps.gr>
2022-12-06 10:32:47 +0200

    Refactor prolects folder structure

--- src/components/Shared/ActionButton.vue
+++ src/components/Shared/ActionButton.vue
@@ -31,11 +31,11 @@ export default {
 
 <style lang="postcss" scoped>
 button {
-  @apply rounded px-5 py-3 font-medium;
+  @apply px-5 py-3 font-medium;
 }
 
 .primary {
-  @apply border-0 bg-brand-blue-1 text-white hover:shadow-blue;
+  @apply rounded bg-brand-blue-1 text-white hover:shadow-blue;
 }
 .secondary {
   @apply bg-transparent text-brand-blue-1  hover:bg-brand-blue-2 hover:text-white;
