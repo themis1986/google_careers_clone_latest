2881c6f2bc7ddf16bc5655e2c24790ab08e6cb57
thiakovidis <thiakovidis@agroapps.gr>
2022-12-06 15:47:59 +0200

    Add pagination using query params, fix tests failing

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="flex-auto bg-brand-gray-2 p-8">
     <ol>
-      <job-listing v-for="job in jobs" :key="job.id" :job="job" />
+      <job-listing v-for="job in displayedJobs" :key="job.id" :job="job" />
     </ol>
   </main>
 </template>
@@ -17,6 +17,15 @@ export default {
     return {
       jobs: [],
     };
+  },
+  computed: {
+    displayedJobs() {
+      const pageString = this.$route.query.page || "1";
+      const pageNumber = Number.parseInt(pageString);
+      const firstJobIndex = (pageNumber - 1) * 10;
+      const lastJobIndex = pageNumber * 10;
+      return this.jobs.slice(firstJobIndex, lastJobIndex);
+    },
   },
   async mounted() {
     const response = await axios.get("http://localhost:3000/jobs");
