c4503c1118f3b5f97ded56f7c41b52e101a137ee
thiakovidis <thiakovidis@agroapps.gr>
2022-12-06 13:22:20 +0200

    Add test for job search form component

--- src/components/JobSearch/JobSearchForm.vue
+++ src/components/JobSearch/JobSearchForm.vue
@@ -6,16 +6,20 @@
     <font-awesome-icon :icon="['fas', 'search']" class="ml-4 mr-3" />
     <div class="flex h-full flex-1 flex-nowrap text-base font-light">
       <div class="relative flex h-full flex-1 items-center pr-3">
-        <label class="absolute left-0 -top-10" for="">Role</label>
-        <text-input v-model="role" placeholder="Software Engineer" />
+        <label class="absolute left-0 -top-10" for="role">Role</label>
+        <text-input id="role" v-model="role" placeholder="Software Engineer" />
       </div>
       <span
         class="flex h-full items-center border-x border-brand-gray-3 bg-brand-gray-2 px-3"
         >in</span
       >
       <div class="relative flex h-full flex-1 items-center pl-3">
-        <label class="absolute left-0 -top-10" for="">Where?</label>
-        <text-input v-model="location" placeholder="Los Angeles" />
+        <label class="absolute left-0 -top-10" for="location">Where?</label>
+        <text-input
+          id="location"
+          v-model="location"
+          placeholder="Los Angeles"
+        />
       </div>
     </div>
     <action-button text="Search" type="secondary" class="rounded-r-3xl" />
