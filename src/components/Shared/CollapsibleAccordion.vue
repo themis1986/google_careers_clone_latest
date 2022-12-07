770aebd42075d8526c9eda771f831bfdc912b0e1
thiakovidis <thiakovidis@agroapps.gr>
2022-12-07 12:01:59 +0200

    Create Accordion base component

---
+++ src/components/Shared/CollapsibleAccordion.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="border-b border-solid border-brand-gray-2 py-5">
+    <div
+      class="flex cursor-pointer flex-wrap items-center justify-between"
+      @click="open"
+    >
+      <h3 class="text-base font-semibold">Organizations</h3>
+      <font-awesome-icon :icon="carrotIcon" />
+    </div>
+    <div v-if="isOpen" class="mt-5 w-full">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CollabsibleAccordion",
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  computed: {
+    carrotIcon() {
+      return this.isOpen ? ["fas", "angle-up"] : ["fas", "angle-down"];
+    },
+  },
+  methods: {
+    open() {
+      this.isOpen = !this.isOpen;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/main.js
+++ src/main.js
@@ -6,13 +6,19 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 /* import font awesome icon component */
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 /* import specific icons */
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSearch,
+  faAngleUp,
+  faAngleDown,
+} from "@fortawesome/free-solid-svg-icons";
 
 import "@/index.css";
 import router from "@/router";
 
 /* add icons to the library */
 library.add(faSearch);
+library.add(faAngleUp);
+library.add(faAngleDown);
 
 const pinia = createPinia();
 
