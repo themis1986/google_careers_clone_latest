278544355c84b22b76cc9ac90504f1f76b6b3b38
thiakovidis <thiakovidis@agroapps.gr>
2022-12-07 15:34:03 +0200

    Use spotlight component, add tests

--- src/components/JobSearch/TheHero.vue
+++ src/components/JobSearch/TheHero.vue
@@ -1,6 +1,6 @@
 <template>
-  <main>
-    <section class="flex h-screen flex-col pt-10 pb-20">
+  <main class="flex h-screen flex-col">
+    <section class="pt-10 pb-20">
       <div class="grid grid-cols-12">
         <div class="col-span-1 col-start-1"></div>
         <div class="col-span-5 col-start-2">
@@ -17,15 +17,35 @@
         <div class="col-span-1 col-start-12"></div>
       </div>
     </section>
+    <spot-light class="flex flex-row justify-center pb-16">
+      <template #default="{ img, title, description }">
+        <router-link
+          to="/jobs/results"
+          class="mx-5 flex h-96 w-72 flex-col rounded-lg bg-brand-gray-2"
+        >
+          <img class="object-contain" :src="img" alt="" />
+          <div class="mt-3 h-48 px-6 py-4">
+            <h3 class="text-lg font-medium">{{ title }}</h3>
+            <p class="mt-3 text-sm">{{ description }}</p>
+          </div>
+          <router-link
+            class="px-6 pb-4 text-sm text-brand-blue-1"
+            to="/jobs/results"
+            >See jobs</router-link
+          >
+        </router-link>
+      </template>
+    </spot-light>
   </main>
 </template>
 
 <script>
 import TheHeadline from "@/components/JobSearch/TheHeadline.vue";
 import JobSearchForm from "@/components/JobSearch/JobSearchForm.vue";
+import SpotLight from "./SpotLight.vue";
 
 export default {
   name: "TheHero",
-  components: { TheHeadline, JobSearchForm },
+  components: { TheHeadline, JobSearchForm, SpotLight },
 };
 </script>
