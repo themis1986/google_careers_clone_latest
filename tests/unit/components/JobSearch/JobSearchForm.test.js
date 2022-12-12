e36fce75b94b958510e726e7178fa517f2a07381
thiakovidis <thiakovidis@agroapps.gr>
2022-12-12 13:48:58 +0200

    Refactor job search form to script setup

--- tests/unit/components/JobSearch/JobSearchForm.test.js
+++ tests/unit/components/JobSearch/JobSearchForm.test.js
@@ -1,20 +1,19 @@
 import { render, screen } from "@testing-library/vue";
 import userEvent from "@testing-library/user-event";
-
+import { useRouter } from "vue-router";
 import JobSearchForm from "@/components/JobSearch/JobSearchForm.vue";
+
+vi.mock("vue-router");
 
 describe("JobSearchForm", () => {
   describe("when user submits form", () => {
     it("directs user to job results page with user's search parameters", async () => {
       const push = vi.fn();
-      const $router = { push };
+      useRouter.mockReturnValue({ push });
       render(JobSearchForm, {
         global: {
           stubs: {
             FontAwesomeIcon: true,
-          },
-          mocks: {
-            $router,
           },
         },
       });
