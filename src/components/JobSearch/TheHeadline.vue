01e7a4cc9775b372aa692a92375a49325cf76464
thiakovidis <thiakovidis@agroapps.gr>
2022-12-13 16:01:26 +0200

    Refactor headline component to script setup with ts

--- src/components/JobSearch/TheHeadline.vue
+++ src/components/JobSearch/TheHeadline.vue
@@ -9,37 +9,26 @@
   </section>
 </template>
 
-<script>
-import nextElementInList from "@/utils/nextElementInList.ts";
+<script lang="ts" setup>
+import nextElementInList from "@/utils/nextElementInList";
+import { ref, computed, onMounted, onBeforeUnmount } from "vue";
 
-export default {
-  name: "TheHeadline",
-  data() {
-    return {
-      action: "Build",
-      interval: null,
-    };
-  },
-  computed: {
-    actionClasses() {
-      return { [this.action.toLowerCase()]: true };
-    },
-  },
-  created() {
-    this.changeTitle();
-  },
-  beforeUnmount() {
-    clearInterval(this.interval);
-  },
-  methods: {
-    changeTitle() {
-      this.interval = setInterval(() => {
-        const actions = ["Build", "Create", "Design", "Code"];
-        this.action = nextElementInList(actions, this.action);
-      }, 3000);
-    },
-  },
+const action = ref("Build");
+const interval = ref<ReturnType<typeof setInterval>>();
+
+const actionClasses = computed(() => {
+  return { [action.value.toLowerCase()]: true };
+});
+
+const changeTitle = () => {
+  interval.value = setInterval(() => {
+    const actions = ["Build", "Create", "Design", "Code"];
+    action.value = nextElementInList(actions, action.value);
+  }, 3000);
 };
+
+onMounted(changeTitle);
+onBeforeUnmount(() => clearInterval(interval.value));
 </script>
 
 <style scoped>
