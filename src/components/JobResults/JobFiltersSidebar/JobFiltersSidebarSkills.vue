dedad80b2d37d30f9830f8161000ae43169fe08a
thiakovidis <thiakovidis@agroapps.gr>
2022-12-16 17:06:35 +0200

    Add searching by skills functionality to component

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarSkills.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarSkills.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="mt-2">
     <input
+      v-model="skillsSearchTerm"
       type="text"
       class="h-12 w-full rounded border border-solid border-brand-gray-1 p-3 text-base shadow-gray"
       placeholder="Computer programming, Finance degree"
@@ -8,6 +9,19 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useUserStore } from "@/stores/user";
+import { computed } from "vue";
+
+const userStore = useUserStore();
+const skillsSearchTerm = computed({
+  get() {
+    return userStore.skillsSearchTerm;
+  },
+  set(value: string) {
+    userStore.UPDATE_SKILLS_SEARCH_TERM(value);
+  },
+});
+</script>
 
 <style scoped></style>
